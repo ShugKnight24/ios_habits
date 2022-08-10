15debd074d50f9aaefb8412443543e27c3007db3
Shugmi Shumunov <SShumunov@gmail.com>
2022-08-09 20:21:00 -0400

    feat: Add New Habits UI

--- Shared/Views/Home.swift
+++ Shared/Views/Home.swift
@@ -7,7 +7,40 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text("Hello World Test!")
+        VStack(spacing: 0){
+            Text("Habits")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .trailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "gearshape")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                    }
+                }
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing: 15){
+                    // Add Habits Button
+                    Button {
+                        
+                    } label: {
+                        Label {
+                            Text("New Habit")
+                        } icon: {
+                            Image(systemName: "plus.circle")
+                        }
+                        .font(.callout.bold())
+                        .foregroundColor(.white)
+                    }
+                    .padding(.top, 15)
+                }
+                .padding(.vertical)
+            }
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
     }
 }
 
