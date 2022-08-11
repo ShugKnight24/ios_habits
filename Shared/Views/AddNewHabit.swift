b216743a2fa0c5744642d2cbb587fb6a29a2b57b
Shugmi Shumunov <SShumunov@gmail.com>
2022-08-11 16:15:28 -0400

    fix: Display Nav title correctly

--- Shared/Views/AddNewHabit.swift
+++ Shared/Views/AddNewHabit.swift
@@ -8,53 +8,55 @@ import SwiftUI
 struct AddNewHabit: View {
     @EnvironmentObject var habitModel: HabitViewModel
     var body: some View {
-        VStack(spacing: 15){
-            TextField("Title", text: $habitModel.title)
-                .padding(.horizontal)
-                .padding(.vertical, 15)
-                .background(Color("black").opacity(0.6),
-                    in: RoundedRectangle(cornerRadius: 9, style: .continuous)
-                )
-            
-            HStack(spacing: 0){
-                ForEach(1...7, id: \.self){
-                    index in let color = "color-\(index)"
-                    Circle()
-                        .fill(Color(color))
-                        .frame(width: 30, height: 30)
-                        .overlay(content: {
-                            if color == habitModel.habitColor{
-                                Image(systemName: "checkmark")
-                                    .font(.caption.bold())
+        NavigationView{
+            VStack(spacing: 15){
+                TextField("Title", text: $habitModel.title)
+                    .padding(.horizontal)
+                    .padding(.vertical, 15)
+                    .background(Color("black").opacity(0.6),
+                        in: RoundedRectangle(cornerRadius: 9, style: .continuous)
+                    )
+                
+                HStack(spacing: 0){
+                    ForEach(1...7, id: \.self){
+                        index in let color = "color-\(index)"
+                        Circle()
+                            .fill(Color(color))
+                            .frame(width: 30, height: 30)
+                            .overlay(content: {
+                                if color == habitModel.habitColor{
+                                    Image(systemName: "checkmark")
+                                        .font(.caption.bold())
+                                }
+                            })
+                            .onTapGesture {
+                                withAnimation {
+                                    habitModel.habitColor = color
+                                }
                             }
-                        })
-                        .onTapGesture {
-                            withAnimation {
-                                habitModel.habitColor = color
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
             }
-        }
-        .frame(maxHeight: .infinity, alignment: .top)
-        .padding()
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationTitle("Add New Habit")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading){
-                Button {
-                    
-                } label: {
-                    Image(systemName: "xmark.circle")
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Add New Habit")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "xmark.circle")
+                    }
+                    .tint(.white)
                 }
-                .tint(.white)
-            }
-            ToolbarItem(placement: .navigationBarTrailing){
-                Button("Done") {
-                    
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button("Done") {
+                        
+                    }
+                    .tint(.white)
                 }
-                .tint(.white)
             }
         }
     }
